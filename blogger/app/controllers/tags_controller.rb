d16b2a6f39434e1bd1873692aff92bc44e85e410
Daniel <bilidanek@gmail.com>
2018-07-08 18:49:30 +0200

    Tagging feature, delete  does not work

---
+++ blogger/app/controllers/tags_controller.rb
@@ -0,0 +1,15 @@
+class TagsController < ApplicationController
+	
+	def show
+		@tag = Tag.find(params[:id])
+	end
+
+	def index
+		@tags = Tag.all
+	end
+
+	def destroy
+		@tag = Tag.find (params[:id])
+		Tag.all.delete(@tag)
+	end
+end
